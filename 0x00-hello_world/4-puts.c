148c4d37e767eb4f3c9e082d8e9344611c5726fd
gitau <dennosea@gmail.com>
2023-02-16 14:13:14 -0800

    c progr

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,10 @@
 #include<stdio.h>
-int main (){
-	puts("\"Programming is like building a multilingual puzzle");
-		return (0);
+/**
+ * main - A program printing line
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
