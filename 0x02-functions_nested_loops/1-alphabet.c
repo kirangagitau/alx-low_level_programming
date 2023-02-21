b218607e60d104dc54bb72f61afeb75df706e5e3
gitau <dennosea@gmail.com>
2023-02-21 12:40:25 -0800

    insomnia

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Aprogram for lower case letters
+ *
+ * Return: always 0
+ */
+
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+	return (0);
+}
