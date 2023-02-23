0f40557a575acb57e349fe0c0f539fe91221d041
gitau <dennosea@gmail.com>
2023-02-23 11:56:37 -0800

    the put char function

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,15 @@
+#include "mani.h"
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to the stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned and errno is set appropriaately
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
