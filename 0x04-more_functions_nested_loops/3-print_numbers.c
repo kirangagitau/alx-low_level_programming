cf35450ac73cee981f57954539f054d3e248adfa
gitau <dennosea@gmail.com>
2023-02-23 13:59:08 -0800

    the num speka

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - print the  numbers.
+ */
+
+void print_numbers(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		printf("%d", x);
+	}
+	_putchar('\n');
+}
