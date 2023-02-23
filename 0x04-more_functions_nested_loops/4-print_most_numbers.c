706556ad02b1dc6c650e7ec2b933ae2a782defd3
gitau <dennosea@gmail.com>
2023-02-23 14:23:47 -0800

    print with exclusion

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - print the numerals 0 to 9.
+ *
+ */
+
+void print_most_numbers(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++);
+	{
+		if (!(C == '2' || c == '4'))
+			_putchar(c);
+	}
+	_putchar('\n');
+}
