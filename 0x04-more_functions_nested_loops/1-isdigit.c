8afd7c4cd11b1bdde44014c37456e99eb18806dd
gitau <dennosea@gmail.com>
2023-02-23 12:42:51 -0800

    check if digit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * _isupper - upper case letter checking.
+ * _isdigit - upper case letter checking.
  * @c: character to check case
  *
  * Return: 0  || 1
  */
 
-int _isupper(int c)
+int _isdigit(int c)
 {
 	if (c >= '0' && c <= '9')
 		return (1);
