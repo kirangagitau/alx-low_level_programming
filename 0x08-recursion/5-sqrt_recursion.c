f9c12597297e7abc6a09cb8aba666e5b2b0db64c
kirangagitau <dennosea@gmail.com>
2023-07-05 23:07:13 +0000

    recursions

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,30 +1,36 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
+
+int root(int n, int i);
 
 /**
- * _sqrt_recursion - function to get the square root of given number
+ * _sqrt_recursion - function returning the number's root
+ * @n: to be calculated root for
  *
- * @n: integer to get sqrt
- *
- * Return: the final answer.
+ * Return: Always (0).
  *
  **/
 
 int _sqrt_recursion(int n)
 {
-	if (n >= 1)
-	{
-		double x, f, i;
-
-		x = sqrt((double)n);
-		f = modf(x, &i);
-		if (f > 0)
-			return (-1);
-		else
-			return ((int)x);
-	}
-	else
+	if (n <= 0)
 		return (-1);
+	return (root(n, 0));
+}
+
+
+/**
+ * root - function doing the recursion to find the root
+ * @n: to be calculated root for
+ * @i: value for iteration counts
+ *
+ * Return: root or error
+ **/
+
+int root(int n, int i)
+{
+	if (i * i > n)
+		return (-1);
+	if (i * i == n)
+		return (i);
+	return (root(n, i + 1));
 }
