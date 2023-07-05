3f55cc70f8a1256dc572a68816bd73e2bd32381e
kirangagitau <dennosea@gmail.com>
2023-07-05 23:57:56 +0000

    recursions

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,7 +1,5 @@
 #include "main.h"
 
-int prime(int n, int j);
-
 /**
  * is_prime_number - checks if given number is prime or not
  * @n: the number checking if prime
@@ -11,25 +9,16 @@ int prime(int n, int j);
 
 int is_prime_number(int n)
 {
-	if (n <= 1)
-		return (0);
-	return (prime(n, n - 1));
-}
+	int prime;
 
-/**
- * prime - checks given value to see if prime.
- * @n: the number checing if prime
- * @j: value for doing the iterations
- *
- * Return: 1 for prime, 0 otherwise.
- *
- **/
-
-int prime(int n, int j)
-{
-	if (j == 1)
+	prime = 1;
+	if ((n % 2) == 0)
+	{
+		prime = 0;
+	}
+	if (prime == 1 )
 		return (1);
-	if (n % i == 0 && i > 0)
+	else
 		return (0);
-	return (prime(n, i - 1));
+	return (exit_success);
 }
