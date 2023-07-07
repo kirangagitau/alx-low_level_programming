611b65406ec549863a326b5ed3f0d829eeb8dd01
kirangagitau <dennosea@gmail.com>
2023-07-07 16:31:14 +0000

    my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point to program
+ * @argc: count of arguments
+ * @argv: vector array of argumets.
+ *
+ * Return: Exit status in the hosted environment
+ *
+ **/
+
+int main(int argc, char **argv)
+{
+	printf("%s\n", argv[0]);
+	return (EXIT_SUCCESS);
+}
