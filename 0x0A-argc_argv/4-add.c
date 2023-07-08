b076ed6eccd403d87b2baa91e66924ce1835a726
kirangagitau <dennosea@gmail.com>
2023-07-08 22:07:35 +0000

    add positive

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+int errmsg(void);
+int checker(char *str);
+
+/**
+ * main - entry point of program
+ * @argc: argument counts
+ * @argv: argument vector
+ *
+ * Return: exit statu of system.
+ **/
+
+int main(int argc, char *argv[])
+{
+	if (argc < 2) /*condition when no arguments are given*/
+	{
+		printf("0\n");
+	}
+	else
+	{
+		int i, abs, ans, counts;
+
+		ans = 0;
+		counts = 1;
+		while (counts < argc) /* traversing all the elements of the argv[] array*/
+		{
+			if (checker(argv[counts]))
+			{
+				abs = atoi(argv[counts]);
+				ans += abs;
+			}
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+			counts++;
+		}
+		printf("%d", ans);
+	}
+	return (EXIT_SUCCESS);
+}
+
+/**
+ * checker - function to check for symbols in array
+ * @str: array passed to function by argv as functions argument.
+ *
+ * Return: 1 for no symbol 0 for symbol.
+ **/
+
+int checker(char *str) /* Checking for symbols in *argv[] */
+{
+	unsigned int counts;
+
+	counts = 0;
+	while (counts < strlen(str))
+	{
+		if (!isdigit(str[counts]))
+		{
+			return (0);
+		}
+		counts++;
+	}
+	return (1);
+}
