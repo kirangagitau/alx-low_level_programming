c47b3d528a09e0b3c7bcb4441872b9bee390e413
kirangagitau <dennosea@gmail.com>
2023-07-17 22:05:17 +0000

    dog will teach you

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,8 +12,6 @@ void print_dog(struct dog *d)
 		return;
 	if (d->name == NULL)
 		d->name = "(nil)";
-	if (d->age == NULL)
-		d->age = "(nill)"
 	if (d->owner == NULL)
 		d->owner = "(nil)";
 	printf("Name: %s\n", d->name);
