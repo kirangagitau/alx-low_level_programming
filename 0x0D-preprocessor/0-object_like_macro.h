122223dca7a7bb291bac13e04719dca54334ee2e
kirangagitau <dennosea@gmail.com>
2023-07-18 13:40:18 +0000

    object like

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,5 +1,5 @@
-#ifndef OBJECT_LIKE_MACRO_H
-#define OBJECT_LIKE_MACRO_H /* inclusion guards */
+#ifndef 0-OBJECT_LIKE_MACRO_H
+#define 0-OBJECT_LIKE_MACRO_H /* inclusion guards */
 
 #define SIZE = 1024
 
