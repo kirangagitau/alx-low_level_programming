98d35ce175f1d61b38dea3ec3a273d92233263a9
kirangagitau <dennosea@gmail.com>
2023-07-18 14:22:32 +0000

    absolute values from macros

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,5 +1,5 @@
 #ifndef FUNCTION_LIKE_MACRO_H
-#define FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H /* inclusion guards */
 
-#define ABS(x) ((x) < (0) ? -(x) : (x))
-#endif
+#define ABS(x) ((x) < (0) ? -(x) : (x)) /* converting value to absolute */
+#endif /* ending the definatio */
