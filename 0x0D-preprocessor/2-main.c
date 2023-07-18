f7d6de6d8c8626554289cc19bf72cd0ff186e77a
kirangagitau <dennosea@gmail.com>
2023-07-18 14:42:00 +0000

    file name compiled from

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,12 +1,13 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <stdio.h> /* facilitate output to screen */
 
 /**
  * main - entry point of programs
+ *
+ * Return: Exit status of the function.
  **/
 
-int main()
+int main(void)
 {
 	printf("%s\n", __FILE__);
-	return(0); /* return type has to be int*/
+	return (0); /* return type has to be int*/
 }
