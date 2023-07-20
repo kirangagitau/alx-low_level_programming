07e6fdb505ba83c6eb74925d83e648804b3a1843
kirangagitau <dennosea@gmail.com>
2023-07-20 20:55:53 +0000

    strings

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_strings - variadic func to rint string.
+ * @separator: the string sep
+ * @n: args count
+ *
+ **/
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	char *str;
+	unsigned int i;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(args, char *);
+
+		if (str == NULL)
+			printf("(nil)");
+		else
+			printf("%s", str);
+
+		if (i != (n - 1) && separator != NULL)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(args);
+}
