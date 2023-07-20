6ad40913e67d76b4907c58d8e37fd5f3b2755533
kirangagitau <dennosea@gmail.com>
2023-07-20 20:20:45 +0000

    One womans constant is another womans variable

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_numbers - variadic func to pritn numbers
+ * @separator: num sepeararot.
+ * @n: count of numbers to give.
+ *
+ **/
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i;
+
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(args, int));
+		if (i != (n - 1) && separator != NULL)
+			printf("%s", separator);
+	}
+	va_end(args);
+	printf("\n");
+}
