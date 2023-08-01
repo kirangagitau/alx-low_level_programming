276a5f8be6e0017c45d4b4847697ecca9b777c14
kirangagitau <dennosea@gmail.com>
2023-08-01 22:38:23 +0300

    return index of node

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - return node at given index
+ * @head: 1st element
+ * @index: index of node to return
+ *
+ * Return: pointe to node || null
+ **/
+list_t *get_nodeint_at_index(list_t *head, unsigned int index)
+{
+	unsigned int j=0;
+	list_t *tem = head;
+
+	while (temp && j < index)
+	{
+		temp = temp->next;
+		j++;
+	}
+	return (temp ? temp:NULL);
+}
