90cf1d855293b969eca1eadbf4d922f47a7f93a7
kirangagitau <dennose@gmail.com>
2023-10-30 23:16:58 +0300

    the elf

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,15 @@
+#include <elf.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ * main - entry point of the program
+ * Return: 0 on success || Exit code 98 on failure
+ **/
+
+int main(void)
+{
+
+	return (0);
+}
